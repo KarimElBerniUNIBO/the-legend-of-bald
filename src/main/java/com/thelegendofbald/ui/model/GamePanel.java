a5595cfc384dfecfb19b7fde9025212102b04809
Karim El Berni <karim.elberni@studio.unibo.it>
2025-04-08 11:12:29 +0200

    Added Bald main image

--- src/main/java/com/thelegendofbald/ui/model/GamePanel.java
+++ src/main/java/com/thelegendofbald/ui/model/GamePanel.java
@@ -2,15 +2,37 @@ package com.thelegendofbald.ui.model;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
 
 public class GamePanel extends JPanel {
+    
+    private BufferedImage image;
 
     public GamePanel(Dimension size) {
         this.setPreferredSize(size);
         this.setBackground(Color.BLACK);
+        loadImage();
     }
 
-    @Override
+    private void loadImage() {
+        try {
+            // Load image from resources folder (adjust path as needed)
+            InputStream is = getClass().getResourceAsStream("/images/character.png");
+            if (is != null) {
+                image = ImageIO.read(is);
+            } else {
+                System.err.println("Image file not found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         
