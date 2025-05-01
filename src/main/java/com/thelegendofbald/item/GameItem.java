dce9c2595a4c90603e26b836eb09892d270bfcd1
Vincent Rey Ramos <vramos120304gmail.com>
2025-05-01 17:56:31 +0200

    render method added to GameItem

--- src/main/java/com/thelegendofbald/item/GameItem.java
+++ src/main/java/com/thelegendofbald/item/GameItem.java
@@ -1,5 +1,7 @@
 package com.thelegendofbald.item;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -7,7 +9,7 @@ import javax.imageio.ImageIO;
 
 public class GameItem {
 
-    protected int x;
+    protected int x; 
     protected int y;
     protected String name;
     protected BufferedImage sprite;
@@ -26,6 +28,15 @@ public class GameItem {
             sprite = ImageIO.read(getClass().getResourceAsStream(path));
         } catch(IOException e ){
             e.printStackTrace();
+        }
+    }
+
+    public void render(Graphics g){
+        if(sprite != null){
+            g.drawImage(sprite,x,y,50,50,null);
+        } else {
+            g.setColor(Color.BLUE);
+            g.fillRect(x, y, 50,50);
         }
     }
 
