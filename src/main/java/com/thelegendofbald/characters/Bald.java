90dc972c32125545893511caf5f271594fbc65d1
LucaDellasantinaUNIBO <luca.dellasantina@studio.unibo.it>
2025-05-28 17:13:53 +0200

    cambiato il metodo di gestione delle mappe

--- src/main/java/com/thelegendofbald/characters/Bald.java
+++ src/main/java/com/thelegendofbald/characters/Bald.java
@@ -2,18 +2,23 @@ package com.thelegendofbald.characters;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
+import com.thelegendofbald.ui.view.TileMap;
+
 public class Bald extends Entity {
+    public static final String SpeedX = null;
+    private TileMap tileMap;
     private int attackPower; // Potenza d'attacco
     private BufferedImage image;
     private String path = "/images/bald.png"; // Percorso dell'immagine
-    private int speedX = 0; // Velocità lungo l'asse X
-    private int speedY = 0; // Velocità lungo l'asse Y
+    public int speedX = 0; // Velocità lungo l'asse X
+    public int speedY = 0; // Velocità lungo l'asse Y
     private BufferedImage[] runFrames; // Array di immagini per l'animazione della corsa
     private int currentFrame = 0; // Indice del frame corrente
     private int frameDelay = 5; // Numero di aggiornamenti prima di cambiare frame
@@ -25,6 +30,19 @@ public class Bald extends Entity {
         this.attackPower = attackPower;
 
         loadRunFrames();
+    }
+
+    public void setTileMap(TileMap tileMap) {
+        this.tileMap = tileMap;
+    }    
+
+    public void setSpawnPosition(int spawnTileId) {
+        // Usa il metodo findSpawnPoint per ottenere le coordinate del tile con ID spawnTileId
+        Point spawnPoint = tileMap.findSpawnPoint(spawnTileId);
+        if (spawnPoint != null) {
+            this.setX(spawnPoint.x); // Imposta la posizione X di Bald
+            this.setY(spawnPoint.y); // Imposta la posizione Y di Bald
+        }
     }
 
     private void loadRunFrames() {
@@ -83,10 +101,10 @@ public class Bald extends Entity {
         if (runFrames != null && runFrames[currentFrame] != null) {
             if (facingRight) {
                 // Disegna normalmente se Bald è girato verso destra
-                g.drawImage(runFrames[currentFrame], x, y, 50, 50, null);
+                g.drawImage(runFrames[currentFrame], x, y, getWidth(), getHeight(), null);
             } else {
                 // Disegna riflettendo l'immagine orizzontalmente
-                g.drawImage(runFrames[currentFrame], x + 50, y, -50, 50, null);
+                g.drawImage(runFrames[currentFrame], x + 50, y, -50, getHeight(), null);
             }
         } else {
             // Fallback al quadrato rosso se i frame non sono caricati
@@ -110,9 +128,18 @@ public class Bald extends Entity {
         } else if (speedX < 0) {
             facingRight = true; // Bald si muove verso sinistra
         }
+    
+        // Spostamento
         this.x += speedX;
         this.y += speedY;
-        
+    }
+
+    public int getHeight() {
+        return 50;
+    }
+
+    public int getWidth() {
+        return 50;
     }
 
 }
--- src/main/java/com/thelegendofbald/ui/view/GamePanel.java
+++ src/main/java/com/thelegendofbald/ui/view/GamePanel.java
@@ -1,8 +1,8 @@
 package com.thelegendofbald.ui.view;
 
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.HashSet;
@@ -20,13 +20,13 @@ public class GamePanel extends JPanel {
     private final DummyEnemy dummyenemy = new DummyEnemy(500, 200, 50, "ZioBilly", 50);
     private final GridPanel gridPanel;
     private final TileMap tileMap;
+    private String currentMapName = "map_1";
 
     Timer timer = new Timer(16, e -> update());
     private final Set<Integer> pressedKeys = new HashSet<>();
 
     public GamePanel(Dimension size) {
         this.setPreferredSize(size);
-        this.setBackground(Color.BLACK);
         this.setFocusable(true);
         this.setLayout(null);
 
@@ -34,8 +34,16 @@ public class GamePanel extends JPanel {
         this.gridPanel.setOpaque(false);
         this.gridPanel.setBounds(0, 0, size.width, size.height);
         this.add(gridPanel);
-
         this.tileMap = new TileMap(size.width, size.height);
+        tileMap.changeMap("map_1");  
+
+        bald.setTileMap(tileMap);
+        Point spawn = tileMap.findSpawnPoint(5);
+        if (spawn != null) {
+            bald.setX(spawn.x);
+            bald.setY(spawn.y);
+}
+        
         this.requestFocusInWindow();
 
         timer.start();
@@ -66,15 +74,74 @@ public class GamePanel extends JPanel {
     private void update() {
         bald.move();
         dummyenemy.followPlayer(bald);
-        dummyenemy.updateAnimation();
+        dummyenemy.updateAnimation();    
+
+        int baldX = bald.getX();
+        int baldY = bald.getY();
+        int baldW = bald.getWidth();
+        int baldH = bald.getHeight();
+        int tileSize = tileMap.TILE_SIZE;
+
+        // Calcolo i tile occupati dal rettangolo del personaggio
+        int leftTile = baldX / tileSize;
+        int rightTile = (baldX + baldW - 1) / tileSize;
+        int topTile = baldY / tileSize;
+        int bottomTile = (baldY + baldH - 1) / tileSize;
+
+        for (int x = leftTile; x <= rightTile; x++) {
+            for (int y = topTile; y <= bottomTile; y++) {
+                if (x >= 0 && x < tileMap.getMapWidthInTiles() && y >= 0 && y < tileMap.getMapHeightInTiles()) {
+                    Tile tile = tileMap.getTileAt(x, y);
+                    if (tile != null) {
+                        System.out.println("Tile at (" + x + "," + y + ") = " + tile + ", id = " + tile.getId());
+                    } else {
+                        System.out.println("Tile at (" + x + "," + y + ") is null");
+                    }
+                    if (tile != null && tile.getId() == 4) {
+                        switchToNextMap();
+                        return;  // esce subito dall'update dopo aver cambiato mappa
+                    }
+                }
+            }
+        }
+
         repaint();
     }
+
+
+    public void changeMap(String mapName) {
+        currentMapName = mapName;
+        tileMap.changeMap(mapName);
+        bald.setTileMap(tileMap);
+    
+        Point spawnPoint = tileMap.findSpawnPoint(5);
+        if (spawnPoint != null) {
+            int tileSize = tileMap.TILE_SIZE;
+
+            bald.setX(spawnPoint.x + tileSize / 2 - bald.getWidth() / 2);
+            bald.setY(spawnPoint.y + tileSize / 2 - bald.getHeight() / 2);
+        }
+
+
+
+    }
+
+    private void switchToNextMap() {
+        if (currentMapName.equals("map_1")) {
+            changeMap("map_2");
+        } else if (currentMapName.equals("map_2")) {
+            changeMap("map_3");
+        } else {
+            System.out.println("Nessuna mappa successiva definita.");
+        }
+    }
+    
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        tileMap.render(g);           
+        tileMap.paint(g);           
         gridPanel.paintComponent(g); 
         bald.render(g);              
         dummyenemy.render(g);        
