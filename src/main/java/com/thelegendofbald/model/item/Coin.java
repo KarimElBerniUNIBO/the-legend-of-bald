ecb091f66896d26ca74450988da551c63e17d33c
Vrasmos <vramos120304@gmail.com>
2025-11-17 12:58:55 +0100

    Fixed gradlew

--- src/main/java/com/thelegendofbald/model/item/Coin.java
+++ src/main/java/com/thelegendofbald/model/item/Coin.java
@@ -1,17 +1,17 @@
 package com.thelegendofbald.model.item;
 
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 import com.thelegendofbald.characters.Bald;
 import com.thelegendofbald.utils.LoggerUtils;
 
+/**
+ * Represents a coin item that can be collected by the player.
+ * When collected, it adds a fixed amount of coins to the player's wallet.
+ */
 public class Coin extends GameItem {
 
     private static final String COIN_NAME = "Coin";
     private static final String COIN_DESCRIPTION = "A shiny gold coin.";
-    private static final int COIN_PRICE = 1;
+    private static final String IMAGE_PATH = "/images/items/gold.png";
     private static final int COIN_SIZE = 32;
     private static final int COIN_VALUE = 20;
 
@@ -23,28 +23,15 @@ public class Coin extends GameItem {
      */
     public Coin(final int x, final int y) {
         super(x, y, COIN_SIZE, COIN_SIZE, COIN_NAME);
-        this.setDescription(COIN_DESCRIPTION);
-        this.setPrice(COIN_PRICE);
-        this.loadSprite();
+        super.setDescription(COIN_DESCRIPTION);
+        super.loadImage(IMAGE_PATH);
     }
-
-    /**
-     * Loads the sprite image for the coin.
-     */
-    private void loadSprite() {
-        try {
-            this.setSprite(ImageIO.read(getClass().getResourceAsStream("/images/items/gold.png")));
-        } catch (IOException e) {
-            LoggerUtils.info("Failed to load coin sprite: " + e.getMessage());
-        }
-    }
-    
     /**
      * Adds the coin's value to the specified wallet.
      *
-     * @param wallet the wallet to add the coin's value to
+     * @param bald the Bald character whose wallet will be credited
      */
-    public void addToWallet(final Bald bald){
+    public void addToWallet(final Bald bald) {
         bald.getWallet().addCoins(Coin.COIN_VALUE);
         LoggerUtils.info(bald.getWallet().getCoins() + " coins in wallet after collecting a coin.");
     }
